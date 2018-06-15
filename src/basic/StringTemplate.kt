3c1269abaf686e52addbb1a9687faa1585277a69
Carlos Ferreira <conta@especializati.com.br>
2018-06-14 21:55:12 -0300

    String Templates Kotlin

---
+++ src/basic/StringTemplate.kt
@@ -0,0 +1,11 @@
+package basic
+
+fun main(args: Array<String>) {
+    var name = "Carlos"
+    var concat = "Hello " + name
+    println(concat)
+
+    var lastName = "Ferreira"
+    var fullName = "$name $lastName"
+    println(fullName)
+}
